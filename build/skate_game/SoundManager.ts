0c95a4cd7c98203d5390369abaf12311ff046a7a
Matt Kay <matthiasklemenc@gmail.com>
2025-11-22 17:01:05 -0600

    Whatever you changed

--- build/skate_game/SoundManager.ts
+++ build/skate_game/SoundManager.ts
@@ -2,58 +2,88 @@
 // Simple HTMLAudioElement-based sound system for the new modular engine
 
 export class SoundManager {
-  music: HTMLAudioElement;
-  coin: HTMLAudioElement;
-  crash: HTMLAudioElement;
+  static music: HTMLAudioElement | null = null;
+  static coin: HTMLAudioElement | null = null;
+  static crash: HTMLAudioElement | null = null;
 
-  jump: HTMLAudioElement;
-  flip: HTMLAudioElement;
-  grind: HTMLAudioElement;
+  static jump: HTMLAudioElement | null = null;
+  static flip: HTMLAudioElement | null = null;
+  static grind: HTMLAudioElement | null = null;
 
-  constructor() {
-    this.music = new Audio("/sounds/music.mp3");
-    this.music.loop = true;
-    this.music.volume = 0.4;
+  static init() {
+    // Initialize all sounds once
+    if (!this.music) {
+      this.music = new Audio("/sounds/music.mp3");
+      this.music.loop = true;
+      this.music.volume = 0.4;
+    }
 
-    this.coin = new Audio("/sounds/coin.wav");
-    this.crash = new Audio("/sounds/crash.wav");
+    if (!this.coin) this.coin = new Audio("/sounds/coin.wav");
+    if (!this.crash) this.crash = new Audio("/sounds/crash.wav");
 
-    this.jump = new Audio("/sounds/jump.wav");
-    this.flip = new Audio("/sounds/flip.wav");
-    this.grind = new Audio("/sounds/grind.wav");
+    if (!this.jump) this.jump = new Audio("/sounds/jump.wav");
+    if (!this.flip) this.flip = new Audio("/sounds/flip.wav");
+    if (!this.grind) this.grind = new Audio("/sounds/grind.wav");
   }
 
-  playMusic() {
-    this.music.currentTime = 0;
-    this.music.play().catch(() => {});
+  static playMusic() {
+    this.init();
+    if (this.music) {
+      this.music.currentTime = 0;
+      this.music.play().catch(() => {});
+    }
   }
 
-  stopMusic() {
-    this.music.pause();
+  static pauseMusic() {
+    if (this.music) this.music.pause();
   }
 
-  playCoin() {
-    this.coin.currentTime = 0;
-    this.coin.play();
+  static resumeMusic() {
+    if (this.music) this.music.play().catch(() => {});
   }
 
-  playCrash() {
-    this.crash.currentTime = 0;
-    this.crash.play();
+  static stopAll() {
+    if (this.music) this.music.pause();
   }
 
-  playJump() {
-    this.jump.currentTime = 0;
-    this.jump.play();
+  static playCoin() {
+    if (this.coin) {
+      this.coin.currentTime = 0;
+      this.coin.play();
+    }
   }
 
-  playFlip() {
-    this.flip.currentTime = 0;
-    this.flip.play();
+  static playCrash() {
+    if (this.crash) {
+      this.crash.currentTime = 0;
+      this.crash.play();
+    }
   }
 
-  playGrind() {
-    this.grind.currentTime = 0;
-    this.grind.play();
+  static playJump() {
+    if (this.jump) {
+      this.jump.currentTime = 0;
+      this.jump.play();
+    }
+  }
+
+  static playFlip() {
+    if (this.flip) {
+      this.flip.currentTime = 0;
+      this.flip.play();
+    }
+  }
+
+  static playGrind() {
+    if (this.grind) {
+      this.grind.currentTime = 0;
+      this.grind.play();
+    }
+  }
+
+  static unlock() {
+    // Required for mobile autoplay unlocking
+    this.playJump();
+    this.pauseMusic();
   }
 }
