13f9cd6e1d87861a1fdb1f8a85d5fe545145fc32
Matt Kay <matthiasklemenc@gmail.com>
2025-11-22 16:52:23 -0600

    Whatever you changed

--- build/skate_game/SoundManager.ts
+++ build/skate_game/SoundManager.ts
@@ -1,42 +1,35 @@
 // SoundManager.ts
-// Clean sound manager with modular triggers
+// Simple HTMLAudioElement-based sound system for the new modular engine
 
 export class SoundManager {
   music: HTMLAudioElement;
-  jump: HTMLAudioElement;
-  flip: HTMLAudioElement;
   coin: HTMLAudioElement;
   crash: HTMLAudioElement;
+
+  jump: HTMLAudioElement;
+  flip: HTMLAudioElement;
   grind: HTMLAudioElement;
 
   constructor() {
     this.music = new Audio("/sounds/music.mp3");
-    this.jump = new Audio("/sounds/jump.wav");
-    this.flip = new Audio("/sounds/flip.wav");
-    this.coin = new Audio("/sounds/coin.wav");
-    this.crash = new Audio("/sounds/crash.wav");
-    this.grind = new Audio("/sounds/grind.wav");
-
     this.music.loop = true;
     this.music.volume = 0.4;
+
+    this.coin = new Audio("/sounds/coin.wav");
+    this.crash = new Audio("/sounds/crash.wav");
+
+    this.jump = new Audio("/sounds/jump.wav");
+    this.flip = new Audio("/sounds/flip.wav");
+    this.grind = new Audio("/sounds/grind.wav");
   }
 
   playMusic() {
+    this.music.currentTime = 0;
     this.music.play().catch(() => {});
   }
 
   stopMusic() {
     this.music.pause();
-  }
-
-  playJump() {
-    this.jump.currentTime = 0;
-    this.jump.play();
-  }
-
-  playFlip() {
-    this.flip.currentTime = 0;
-    this.flip.play();
   }
 
   playCoin() {
@@ -47,6 +40,16 @@ export class SoundManager {
   playCrash() {
     this.crash.currentTime = 0;
     this.crash.play();
+  }
+
+  playJump() {
+    this.jump.currentTime = 0;
+    this.jump.play();
+  }
+
+  playFlip() {
+    this.flip.currentTime = 0;
+    this.flip.play();
   }
 
   playGrind() {
