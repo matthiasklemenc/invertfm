ec7a08790ed0f25a4c56e7d663ff0390b93748bd
Matt Kay <matthiasklemenc@gmail.com>
2025-11-23 00:30:16 -0600

    Cache bust deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
         plugins: [react()],
 
         // GitHub Pages base path
-        base: env.VITE_BASE_PATH || '/invertfm/?v=3/',
+        base: env.VITE_BASE_PATH || '/invertfm/',
 
         resolve: {
             alias: {
