df5a41d5280bcfa21f03e7d8ea8fa03c8bb9ad70
Matt Kay <matthiasklemenc@gmail.com>
2025-11-24 07:10:58 -0600

    Whatever you changed

--- build/skate_game/characters.ts
+++ build/skate_game/characters.ts
@@ -1,28 +1,26 @@
-export const CHARACTERS = [
-  {
-    id: "kai",
-    name: "KAI",
-    img: "/invertfm/skate_game/sprites/player_carousel.png",
-    spritePrefix: "/invertfm/skate_game/sprites/",
-  },
-  {
-    id: "bali",
-    name: "BALI",
-    img: "/invertfm/skate_game/sprites/player_carousel.png",
-    spritePrefix: "/invertfm/skate_game/sprites/",
-  },
-  {
-    id: "dubs",
-    name: "DUBS",
-    img: "/invertfm/skate_game/sprites/player_carousel.png",
-    spritePrefix: "/invertfm/skate_game/sprites/",
-  },
-  {
-    id: "rayssa",
-    name: "RAYSSA",
-    img: "/invertfm/skate_game/sprites/player_carousel.png",
-    spritePrefix: "/invertfm/skate_game/sprites/",
-  }
-] as const;
+/* ============================================================================
+   CORRECT CHARACTER DEFINITIONS WITH CORRECT PATHS
+   ============================================================================ */
 
-export type CharacterId = typeof CHARACTERS[number]["id"];
+export const CHARACTERS = [
+    {
+        id: "kai",
+        name: "Kai",
+        img: "/invertfm/skate_game/sprites/player_carousel.png",
+    },
+    {
+        id: "bali",
+        name: "Bali",
+        img: "/invertfm/skate_game/sprites/player_carousel.png",
+    },
+    {
+        id: "dubs",
+        name: "Dubs",
+        img: "/invertfm/skate_game/sprites/player_carousel.png",
+    },
+    {
+        id: "rayssa",
+        name: "Rayssa",
+        img: "/invertfm/skate_game/sprites/player_carousel.png",
+    },
+];
